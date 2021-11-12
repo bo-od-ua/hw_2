6b33ce3173808704e4a2d7837a45fdeb5c73a453
Бояринцев Александр <a.bo@bramblingtech.com>
2021-11-12 13:58:49 +0200

    fix summ.sh

--- summ.sh
+++ summ.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
 
 is_valid(){
-  if ! [[ "$1" =~ ^[0-9\.-]+$ ]] ;
+#  if ! [[ "$1" =~ ^[0-9\.-]+$ ]] ;
+  if ! [[ "$1" =~ ^-?\d+(\.\d+)?$ ]] ;
   then
     echo "err: not numeric"
     exit 1
@@ -17,5 +18,8 @@ is_valid "$b"
 
 echo "===================="
 
-res=`echo $a $b + p | dc`
-echo "$a + $b = $res"
+#res=`echo $a $b + p | dc`
+#echo "$a + $b = $res"
+
+awk -v a="$a" -v b="$b" 'BEGIN{print (a + b)}'
+
